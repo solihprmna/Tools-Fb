e899ba766f28d6b4d66a378297003d49276b3c06
MR.$ <73181684+solihprmna@users.noreply.github.com>
2020-11-16 18:20:46 +0700

    Add files via upload

---
+++ Tools-Fb.py
@@ -0,0 +1,126 @@
+#NgodingbySolihPermana
+#MR.$
+#Boleh Record Asal Cantumkan Nama Author
+#Hargai Karya Saya :)
+
+import os,sys,time
+
+
+def bersih():
+     os.system("clear")
+
+def main():
+    time.sleep(1)
+    bersih()
+    time.sleep(1)
+    print ('\33[1;33m                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            Hello \33[31;1mBangsat \33[0;36m:)  ')
+    time.sleep(1)
+    bersih()
+    print ('\33[32;1m')
+    os.system ('figlet Tools Crack FB')
+    print'\33[36;1m+\33[1;33m===========================================\33[36;1m+'
+    print'\33[36;1mAuthor    \33[31;1m: \33[1;33mSolih Permana '
+    print'\33[36;1mYoutube   \33[31;1m: \33[1;33mSolih Tutorial 02'
+    print'\33[36;1mFacebook  \33[31;1m: \33[1;33mSolih Permana'
+    print'\33[36;1mInstagram \33[31;1m: \33[1;33m@solihprmn02_'
+    print'\33[36;1mGithub    \33[31;1m: \33[1;33mhttps://github.com/solihprmna '
+    print'\33[36;1m+\33[1;33m===========================================\33[36;1m+'
+    print'\33[32;1m++++++++++++  \33[37;1mM E N U \33[32;1m++++++++++++'
+    print'\33[32;1m[\33[37;1m1\33[32;1m] \33[36;1mDark Fb By Sensei {No Login} '
+    print'\33[32;1m[\33[37;1m2\33[32;1m] \33[36;1mMBF By DulLah '
+    print'\33[32;1m[\33[37;1m3\33[32;1m] \33[36;1mCrack FB BY Cindyadelia SR. {Login Via Token} '
+    print'\33[32;1m[\33[37;1m4\33[32;1m] \33[36;1mCrack FB BY RATU ERROR {Login Via Cookies}'
+    print'\33[32;1m[\33[37;1m5\33[32;1m] \33[36;1mFB Crack BY Tech-Abm '
+    print'\33[32;1m[\33[37;1m6\33[32;1m] \33[36;1mUbah Cookies Jadi Token '
+    print'\33[32;1m[\33[31;1m0\33[32;1m] \33[31;1mExit '
+    gans = raw_input ('\33[36;1m==> ')
+    if gans in ['1']:
+        time.sleep(1)
+        os.system('pkg install python2')
+        os.system('pip2 install tqdm')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+        os.system ('git clone https://github.com/BOT-033/Sensei')
+        bersih()
+	time.sleep(2)
+	print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+        os.system('cd Sensei;python2 main.py')
+    if gans in ['2']:
+        time.sleep(1)
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install python git')
+        os.system('pip install requests bs4 futures')
+        os.system('git clone https://github.com/dz-id/smbf')
+        bersih()
+	time.sleep(2)
+        print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+        os.system('cd smbf;python mbf.py')
+	sys.exit
+    if gans in ['3']:
+        time.sleep(1)
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install python2')
+        os.system('pkg install git')
+        os.system('git clone https://github.com/vrhasya/UNIS3X')
+        bersih()
+	time.sleep(2)
+        print'\33[1;33mBERHASIL DIISNSTALL......'
+	bersih()
+        os.system('cd UNIS3X;python2 UNIS3X.py')
+	sys.exit
+    if gans in ['4']:
+        time.sleep(1)
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install git')
+        os.system('pkg install python')
+        os.system('pkg install nodejs')
+        os.system('pip install module colorama futures')
+        os.system('git clone https://github.com/kinarworo/mbf')
+	bersih()
+	time.sleep(2)
+        print'\33[1;33mBERHASIL DIISNSTALL......'
+	bersih()
+	os.system('cd mbf;python mbf.py')
+	sys.exit
+    if gans in ['5']:
+        time.sleep(1)
+        os.system('apt update')
+        os.system('apt upgrade')
+        os.system('apt install git')
+        os.system('apt install python2')
+        os.system('apt install pip2')
+        os.system('git clone https://github.com/Tech-abm/Fb-Crack')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+        bersih()
+	time.sleep(3)
+        print'\33[1;33mBERHASIL DIISNSTALL.......'
+        print ('PASSWORD TOOLS DIBAWAH')
+        print ('Username : Abm')
+        print ('Password : Abm')
+	bersih()
+        os.system('cd Fb-Crack;python2 Crack.py')
+    if gans in ['6']:
+	os.system('pkg update && pkg upgrade')
+	os.system('pkg install python git')
+	os.system('pip install requests')
+	os.system('git clone https://github.com/dz-id/fb_get_token_from_cookie')
+	bersih()
+	time.sleep(2)
+	print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+	os.system('cd fb_get_token_from_cookie;python main.py')
+    if gans in ['0']:
+        time.sleep(2)
+        exit()
+    else:
+        time.sleep(1)
+        print ' Pilih Yang Bener Goblok . . .'
+        time.sleep(1)
+        main()
+
+main()
+        
+
